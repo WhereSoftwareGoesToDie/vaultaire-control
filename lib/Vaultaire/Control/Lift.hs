50a9dae71231ba1ffd0472e3586c00b841aa0369
Tran Ma <tran@defma.in>
2014-10-07 17:01:15 +1100

    lift: docs

--- lib/Vaultaire/Control/Lift.hs
+++ lib/Vaultaire/Control/Lift.hs
@@ -5,11 +5,18 @@
   , FlexibleContexts
   , OverlappingInstances
   #-}
+
+-- * This module uses @OverlappingInstances@ to allow multiple @ReaderT@s in the same stack
+--   (or @StateT@s, or any commutative transformers whose operations can be lifted freely through a stack).
+--   We use this to avoid defining classes for each type of connections, environments, etc. we might
+--   want to perform a Vaultaire query on.
+--   This is probably not the ideal solution, and only works for easy transformers, use with care.
+--
 module Vaultaire.Control.Lift
      ( In(..) )
 where
 
-import           Control.Monad.Trans.Class
+import Control.Monad.Trans.Class
 
 -- | Constraint a transformer stack @haystack@ to have a transformer @needle@ anywhere in the stack.
 --   This relies on @OverlappingInstances@ to find the position of @needle@ in @haystack@,
